b5a7858d136ce4e861955099d2a614954d2b6e83
shahrour <ahmadshahrour951@gmail.com>
2020-09-06 21:49:52 -0700

    Infinite loop and final print

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ def convert(temp_f):
   # this is a helper function that takes the input of temp F  and calculates the output which is temp C
     return ((int(temp_f) - 32) * 5) / 9
 
+
+# I ask for the user to input the data i request
+user_input = input(
+    "Enter the Fahrenheit temperature you want to convert to Celsius: ")
+
+# this will go into an infinite loop of input validation
+while user_input.isdigit() == False:
+    print("Your input was not a number. Please try again.")
+    user_input = input(
+        "Enter the Fahrenheit temperature you want to convert to Celsius: ")
+    continue
+
+# this is where the answer will be printed, that means that the user_input was a number
+print('The conversion is:  {} Celcius'.format(convert(user_input)))
